3e7072f93d5d751d2444799bcacb0b2123681eb2
rajashreesalunke18 <146177383+rajashreesalunke18@users.noreply.github.com>
2024-06-07 17:36:34 +0000

    conversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score="33abc"
+console.log(typeof score)
+let value=Number(score)
+console.log(typeof value)
+console.log(value)
+
+
+//"33"=>33
+//"33abc"=>NaN(Not a Number)
+//true=>1;false=>0
+
+let isLoggedIn=1  
+let booleanIsLoggedIn=Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+//1=>true  0=>false
+//""=>false
+//"rajashree"=>true
+
+
+let x=33
+let stringNo=String(x)
+console.log(stringNo)
