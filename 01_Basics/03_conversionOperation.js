04e11147607c2a3ae5d0fc780b1c6c7f54b4e5fd
rajashreesalunke18 <146177383+rajashreesalunke18@users.noreply.github.com>
2024-06-07 18:26:49 +0000

    conversions

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,1 +1,57 @@
-console.log("1"+2+2)
+let score="Rajashree"
+console.log(typeof score)
+let value=Number(score)
+console.log(typeof value)
+console.log(value)
+
+
+//"33"=>33
+//"33abc"=>NaN(Not a Number)
+//true=>1;false=>0
+
+let isLoggedIn=1  
+let booleanIsLoggedIn=Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+//1=>true  0=>false
+//""=>false
+//"rajashree"=>true
+
+
+let x=33
+let stringNo=String(x)
+console.log(stringNo)
+console.log(typeof stringNo)
+
+
+//*****************OPERATIONS****************/
+let n =3
+let negValue=-n
+console.log(negValue)
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2/2)
+//console.log(2**3
+//console.log(2%2)
+
+
+let str1="hello"
+let str2=" rajashree"
+let str3=str1+str2
+//console.log(str3)
+// console.log("1"+2)
+// console.log("1"+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+console.log(+true )
+let counter=100
+counter++
+console.log(counter)
+
+
+
+
+
