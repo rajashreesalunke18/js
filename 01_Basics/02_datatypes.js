6e418cf2538a34915665395b561456f07c9eb0f2
rajashreesalunke18 <146177383+rajashreesalunke18@users.noreply.github.com>
2024-06-07 14:50:26 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict";// treat all JS code as newer version
+//alert(3+3)//we are using nodejs,not browser
+ 
+//datatypes=>
+//number
+//string
+//bigint
+//boolean
+//null
+//undefined
+//symbol=>unique(react)
+
+//object
+console.log(typeof(null))
+console.log(typeof(defined))
