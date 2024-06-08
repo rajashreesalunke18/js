d955b02d2b9503aec3a9d5f4d88dad17bc50d483
rajashreesalunke18 <146177383+rajashreesalunke18@users.noreply.github.com>
2024-06-08 08:56:01 +0000

    math

---
+++ 01_Basics/04_maths.js
@@ -0,0 +1,9 @@
+console.log(Math.abs(-4))
+console.log(Math.ceil(3.2))
+console.log(Math.floor(4.5))
+console.log(Math.round(5.66))
+console.log(Math.random())//between 0 and 1
+console.log((Math.random()*10)+1)//for value above 1
+const min =10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)//for value in range
